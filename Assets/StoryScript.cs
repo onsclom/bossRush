fd8ead3bc062a1b069d6c1b416d012db11b619e9
Austin Merrick <austinmerrick98@gmail.com>
2020-05-13 18:54:01 -0700

    Finished starting slideshow.

--- Assets/StoryScript.cs
+++ Assets/StoryScript.cs
@@ -5,15 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class StoryScript : MonoBehaviour
 {
-    private Animator animator;
+    private Animator slideshowAnimator;
     public List<string> startups;
     public int count;
+    public Animator clickAnimator;
+    public Animator fadeAnimator;
+    private float timeInIdle;
 
     // Start is called before the first frame update
     void Start()
     {
-        count = 1;
-        animator = gameObject.GetComponent<Animator>();
+        timeInIdle = 0;
+        count = 0;
+        slideshowAnimator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,13 +25,43 @@ public class StoryScript : MonoBehaviour
     {
         if (Input.anyKeyDown)
         {
+            if (count == 0)
+            {
+                print("wow");
+                fadeAnimator.Play("FadeIn");
+            }
+
             count += 1;
-            animator.Play("startup"+count.ToString());
+            slideshowAnimator.Play("startup"+count.ToString());
+            clickAnimator.Play("hidden");
+            timeInIdle = 0;
+        }
+
+        bool proceedPlaying = clickAnimator.GetCurrentAnimatorStateInfo(0).IsName("mouseClick");
+        string curClipName = slideshowAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+
+        if ( (!proceedPlaying && curClipName.Substring(0,4)=="idle"))
+        {
+            timeInIdle += Time.deltaTime;
+
+            if (timeInIdle > 2)
+            {
+                clickAnimator.Play("mouseClick");
+            }
+        }
+
+        if (count == 0)
+        {
+            timeInIdle += Time.deltaTime;
+            if (timeInIdle > 3)
+                clickAnimator.Play("clickToProcee");
         }
     }
 
     public void Done()
     {
+        print("done");
         SceneManager.LoadScene("SampleScene");
+        fadeAnimator.Play("fadeOut");
     }
 }
